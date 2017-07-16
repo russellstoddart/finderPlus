35b653f1b5d56e13d6927b964c7b6c26b6b9987b
Russell Stoddart <russell.stoddart@johnlewis.co.uk>
2017-07-16 13:52:43 +0100

    Initial Commit

---
+++ finderPlus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  finderPlus
+//
+//  Created by Russell Stoddart on 16/07/2017.
+//  Copyright © 2017 Russell Stoddart. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
